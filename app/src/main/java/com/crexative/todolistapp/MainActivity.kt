9bd3f4ccd3f2dafb148803312e5bffbfa817eb2a
Johan Mosquera <joncamilomoni@gmail.com>
2022-10-31 15:57:43 -0500

    AddEditTodo screen, viewModel and events

--- app/src/main/java/com/crexative/todolistapp/MainActivity.kt
+++ app/src/main/java/com/crexative/todolistapp/MainActivity.kt
@@ -3,42 +3,17 @@ package com.crexative.todolistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.crexative.todolistapp.ui.theme.TodoListAppTheme
-import com.crexative.todolistapp.ui.todo_list.TodoListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TodoListAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TodoListAppTheme {
-        Greeting("Android")
     }
 }
