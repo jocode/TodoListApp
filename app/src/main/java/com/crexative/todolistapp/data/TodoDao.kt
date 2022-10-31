057347573391fb9ccbe3660d884c2d6e3f42960b
Johan Mosquera <joncamilomoni@gmail.com>
2022-10-31 10:53:11 -0500

    Todo Repository and dependency injection are created

--- app/src/main/java/com/crexative/todolistapp/data/TodoDao.kt
+++ app/src/main/java/com/crexative/todolistapp/data/TodoDao.kt
@@ -20,6 +20,6 @@ interface TodoDao {
     suspend fun getTodoById(id: Int): Todo?
 
     @Query("SELECT * FROM todo")
-    suspend fun getTodos() : Flow<List<Todo>>
+    fun getTodos() : Flow<List<Todo>>
 
 }
--- app/src/main/java/com/crexative/todolistapp/di/AppModule.kt
+++ app/src/main/java/com/crexative/todolistapp/di/AppModule.kt
@@ -1,4 +1,34 @@
 package com.crexative.todolistapp.di
 
+import android.app.Application
+import androidx.room.Room
+import com.crexative.todolistapp.data.TodoDatabase
+import com.crexative.todolistapp.data.TodoRepository
+import com.crexative.todolistapp.data.TodoRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase) : TodoRepository {
+        return TodoRepositoryImpl(db.dao)
+    }
+
 }
